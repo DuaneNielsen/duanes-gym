d9d3e584e92116dbf388e6e968481cfa350da44b
duanenielsen <duane.nielsen.rocks@gmail.com>
2019-03-21 02:59:17 +0000

    Updated Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='gym_duane',
-      version='0.0.1',
+      version='0.0.2',
       install_requires=['gym>=0.2.3',
                         'pymunk',
                         'pygame',
