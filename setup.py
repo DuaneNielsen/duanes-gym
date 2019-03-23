7517002ded2c3b54419ec8654f9068c65808f669
duanenielsen <duane.nielsen.rocks@gmail.com>
2019-03-23 03:36:01 +0000

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from setuptools import setup, find_packages
 
 setup(name='gym_duane',
-      version='0.0.5',
-      packages=find_packages(include=['gym_duane.*']),
+      version='0.0.6',
+      packages=find_packages(),
       install_requires=['gym>=0.2.3',
                         'pymunk',
                         'pygame',
